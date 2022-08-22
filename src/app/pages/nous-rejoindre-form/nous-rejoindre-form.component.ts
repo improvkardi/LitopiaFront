54be28eb4fc3002a71690e1447325251913f7d62
Vincent CLERC <45265215+RehArk@users.noreply.github.com>
2022-08-22 21:35:40 +0200

    feat : add html for nous-rejoindre-form part 1 and 2

--- src/app/pages/nous-rejoindre-form/nous-rejoindre-form.component.ts
+++ src/app/pages/nous-rejoindre-form/nous-rejoindre-form.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Router} from "@angular/router"
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-nous-rejoindre-form',
   templateUrl: './nous-rejoindre-form.component.html',
   styleUrls: ['./nous-rejoindre-form.component.scss']
 })
+
 export class NousRejoindreFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() id: number;
+  public router: Router;
+
+  constructor(router: Router, route: ActivatedRoute) {
+    this.id = this.getRouteId(route);
+    this.router = router;
+  }
+
+
+  getRouteId(route: ActivatedRoute) {
+
+    let id = route.snapshot.paramMap.get('id');
+
+    if (id == null) {
+      return 0;
+    }
+
+    return parseInt(id);
+    
+  }
 
   ngOnInit(): void {
+    if(
+      this.id < 1 ||
+      this.id > 3
+    ) {
+      this.router.navigate(['/nous-rejoindre'])
+    }
   }
 
 }
