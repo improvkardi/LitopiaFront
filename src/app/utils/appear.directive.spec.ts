e60f95c884c02e0de0d19c0fee9a9decb5162de9
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-08-27 16:37:18 +0200

    Fix : Delete spec that don't work

--- src/app/utils/appear.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AppearDirective } from './appear.directive';
-
-describe('AppearDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AppearDirective();
-    expect(directive).toBeTruthy();
-  });
-});
