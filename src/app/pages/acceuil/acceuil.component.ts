c56736b3a7e91fb30cf7ded88e73a9a97ddebb68
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-08-27 16:29:10 +0200

    Feat : Landing page fully animate yeay !

--- src/app/pages/acceuil/acceuil.component.ts
+++ src/app/pages/acceuil/acceuil.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {upFadeInAnimation} from "../../animations/up-fade-in.animation";
 
 @Component({
   selector: 'app-acceuil',
   templateUrl: './acceuil.component.html',
-  styleUrls: ['./acceuil.component.scss']
+  styleUrls: ['./acceuil.component.scss'],
+  animations:[upFadeInAnimation]
 })
 export class AcceuilComponent implements OnInit {
+  appearSet=new Set<string>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  appear(appearName:string){
+    console.log(appearName+' is appear')
+    this.appearSet.add(appearName)
+  }
+
+  isAppear(appearName:string){
+    return this.appearSet.has(appearName)
   }
 
 }
