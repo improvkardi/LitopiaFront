70aa9d7c267b3dc2c107a85dcf4083e8d2185ca3
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-08-31 17:41:56 +0200

    Feat : first implementation of login with discord

---
+++ src/app/auth/services/auth-user.ts
@@ -0,0 +1,29 @@
+export enum UserRole {
+  GHOST = "ghost",
+  CANDIDATE = "CANDIDATE",
+  BAN = "ban",
+  REFUSED = "refuse",
+  LITOPIEN = "litopien",
+  ACTIVE_LITOPIEN = "active litopien",
+  INACTIVE_LITOPIEN = "inactive litopien",
+  LITOGOD = "litogod",
+  UNIQUE_GOD = "unique god",
+}
+
+export interface logged{
+  logged:boolean,
+}
+
+export interface User extends logged{
+  discordID: string;
+  minecraftUUID: string;
+  discordNickname: string;
+  discordAvatar: string;
+  minecraftNickname: string;
+  role: UserRole;
+  candidature: string;
+  candidatureProposalAt:Date;
+  candidatureAcceptedAt:Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
