1ab5a2f02016f9f5205369c4367f92ccc28b9676
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-09-01 14:30:03 +0200

    Feat : gen open api

---
+++ src/app/api/api/api.ts
@@ -0,0 +1,11 @@
+export * from './auth.service';
+import { AuthService } from './auth.service';
+export * from './candidatureProcess.service';
+import { CandidatureProcessService } from './candidatureProcess.service';
+export * from './default.service';
+import { DefaultService } from './default.service';
+export * from './minecraftUsers.service';
+import { MinecraftUsersService } from './minecraftUsers.service';
+export * from './users.service';
+import { UsersService } from './users.service';
+export const APIS = [AuthService, CandidatureProcessService, DefaultService, MinecraftUsersService, UsersService];
---
+++ src/app/api/model/models.ts
@@ -0,0 +1,4 @@
+export * from './candidaturePostDto';
+export * from './minecraftUserEntity';
+export * from './userEntity';
+export * from './userEntityMinecraftUser';
