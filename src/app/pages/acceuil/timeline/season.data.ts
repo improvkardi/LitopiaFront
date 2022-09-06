b1a36493d70fae95cdb06f208f13e9eb5df1adb4
improvkardi <89781468+improvkardi@users.noreply.github.com>
2022-09-06 10:13:55 +0200

    Correction de quelques fautes
    J'ai également changé quelques tournures de phrases

--- src/app/pages/acceuil/timeline/season.data.ts
+++ src/app/pages/acceuil/timeline/season.data.ts
@@ -5,14 +5,14 @@ export const articles : TimelineArticle[] = [
     title: "Kraftopia",
     displayDate: "Janvier 2015",
     datetime: "2015-01-01 00:00",
-    description: "Kraftopia et le premier serveur. Repris par les plus anciens membres de Litopia.",
+    description: "Kraftopia est le premier serveur. Repris par les plus anciens membres de Litopia.",
     image: ""
   },
   {
     title: "Litopia saison 1",
     displayDate: "Janvier 2017",
     datetime: "2017-01-01 00:00",
-    description: "Après une longue pause Loff demande à Solarius d'ouvrir un nouveau serveur, serveur qui sera nommée Litopia. Cette première saison sera dans un monde amplified.",
+    description: "Après une longue pause Loff demande à Solarius d'ouvrir un nouveau serveur, serveur qui sera nommé Litopia. Cette première saison sera dans un monde amplifié.",
     image:""
   },
   {
@@ -26,35 +26,35 @@ export const articles : TimelineArticle[] = [
     title: "Litopia saison Pirate",
     displayDate: "Juillet 2018",
     datetime: "2018-07-01 00:00",
-    description: "Ça y est on lance la saison 3, 1 ans aprés la saison 2. Au programme nouveau discord, nouveau site, nouveau logo. Pour une saison Pirate",
+    description: "Ça y est, on lance la saison 3. 1 ans aprés la saison 2. Au programme, nouveau discord, nouveau site, nouveau logo. Pour une saison Pirate",
     image: ""
   },
   {
-    title: "Saison oublier",
+    title: "Saison oubliée",
     displayDate: "Unknown 2019",
     datetime: "2019-03-17 14:21:46",
-    description: "Kraftopia et le premier serveur. Repris par les plus anciens membres de Litopia.",
+    description: "CONFIDENTIEL : Saison lancée pendant ~~^[\~{#~\, organisée autour du thème #@`\#~[ `@`#~[`\@#~ #~|~^|[^#~`! élue meilleure saison par 50l4R1U5",
     image: ""
   },
   {
     title: "Une saison de noël !",
     displayDate: "Décembre 2019",
     datetime: "2019-12-20 00:00",
-    description: "Et oui on s'est dit que la nouvelle saison serais une saison de noël ! Noël tous les jours c'est trop bien !",
+    description: "Et oui, on s'était dit que la nouvelle saison serait une saison de noël ! Noël tous les jours, c'est trop bien !",
     image: ""
   },
   {
     title: "Saison Litorona virus!",
     displayDate: "Mars 2020",
     datetime: "2020-03-22 00:00",
-    description: "En fait on était tous confinés alors on a fait une saison de Litopia",
+    description: "En fait, on était tous confinés donc on a fait une nouvelle saison de Litopia",
     image: ""
   },
   {
     title: "Litopia saison 4",
     displayDate: "Juillet 2020",
     datetime: "2020-07-10 00:00",
-    description: "On se refait une vraie saison comme à la bonne époque et ça va être absolument génial",
+    description: "On a refait une vraie saison comme à la bonne époque et se fut absolument génial",
     image:""
   },
   {
@@ -68,7 +68,7 @@ export const articles : TimelineArticle[] = [
     title: "Litopia saison 5",
     displayDate: "Decembre 2021",
     datetime: "2021-12-04 16:00",
-    description: "Saison marqué par une forte rivalité entre la Licorp (les capitaliste) et l'Union Litopiste (les communiste). Une saison de guerre froide dans les montagnes",
+    description: "Saison marquée par une forte rivalité entre la Licorp (les capitalistes) et l'Union Litopiste (les communistes). Une saison de guerre froide dans les montagnes. Conflit à suivre...",
     image:""
   }
 ]
